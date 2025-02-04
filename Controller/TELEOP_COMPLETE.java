47ffa3e69024e4ee2cc686d93a4e55f6d0f66811
Kapana Studios <74673120+KapanaStudios@users.noreply.github.com>
2025-02-04 15:02:21 +0100

    added sliders option, attempt 1

--- Controller/TELEOP_COMPLETE.java
+++ Controller/TELEOP_COMPLETE.java
@@ -7,94 +7,117 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 @TeleOp(name = "TELEOP_COMPLETE")
 public class TELEOP_COMPLETE extends LinearOpMode {
-    // Port Cofigurations & Declearing Components & Type
-    // DIRECT MOTOR PINS
-    private DcMotor rightFront;
-    private DcMotor rightBack;
-    private DcMotor leftFront;
-    private DcMotor leftBack;
-    private DcMotor arm;
-    private DcMotor chain;
+	// Port Cofigurations & Declearing Components & Type
+	// DIRECT MOTOR PINS
+	private DcMotor rightFront;
+	private DcMotor rightBack;
+	private DcMotor leftFront;
+	private DcMotor leftBack;
+	private DcMotor arm;
+	private DcMotor chain;
+	private DcMotor leftSlider;
+	private DcMotor rightSlider;
 
-    double speed =1;
-    @Override
-    public void runOpMode() {
-        // Initialize hardware
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        chain = hardwareMap.get(DcMotor.class, "chain");
+	double speed =1;
+	@Override
+	public void runOpMode() {
+		// Initialize hardware
+		arm = hardwareMap.get(DcMotor.class, "arm");
+		chain = hardwareMap.get(DcMotor.class, "chain");
+		rightSlider = hardwareMap.get(DcMotor.class, "rightSlider");
+		leftSlider = hardwareMap.get(DcMotor.class, "leftSlider");
 
-        // Set motor directions if necessary
-        arm.setDirection(DcMotor.Direction.FORWARD);
-        chain.setDirection(DcMotor.Direction.FORWARD);
+		// Set motor directions if necessary
+		arm.setDirection(DcMotor.Direction.FORWARD);
+		chain.setDirection(DcMotor.Direction.FORWARD);
+		rightSlider.setDirection(DcMotor.Direction.FORWARD);
+		leftSlider.setDirection(DcMotor.Direction.REVERSE);
+		
+		rightSlider.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		leftSlider.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        rightBack = hardwareMap.get(DcMotor.class,"rightBack");
-        leftFront = hardwareMap.get(DcMotor.class,"leftFront");
-        leftBack = hardwareMap.get(DcMotor.class,"leftBack");
-        
-        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        
+		rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+		rightBack = hardwareMap.get(DcMotor.class,"rightBack");
+		leftFront = hardwareMap.get(DcMotor.class,"leftFront");
+		leftBack = hardwareMap.get(DcMotor.class,"leftBack");
+		
+		rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+		
 
-        rightFront.setDirection(DcMotor.Direction.FORWARD);
-        rightBack.setDirection(DcMotor.Direction.REVERSE);
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
+		rightFront.setDirection(DcMotor.Direction.FORWARD);
+		rightBack.setDirection(DcMotor.Direction.REVERSE);
+		leftFront.setDirection(DcMotor.Direction.REVERSE);
+		leftBack.setDirection(DcMotor.Direction.REVERSE);
 
-        waitForStart();
+		waitForStart();
 
-        while (opModeIsActive()) {
-            float clawMainPower = gamepad1.left_trigger - gamepad1.right_trigger;
-            float clawChainPower = 0;
+		while (opModeIsActive()) {
+			float clawMainPower = gamepad1.left_trigger - gamepad1.right_trigger;
+			float clawChainPower = 0;
+			float sliderPower = 0;
 
-            // Control chain motor with bumpers
-            if (gamepad1.left_bumper) {
-                clawChainPower = 0.2f;
-            } else if (gamepad1.right_bumper) {
-                clawChainPower = -0.2f;
-            } else {
-                clawChainPower = 0;
-            }
+			// Control chain motor with bumpers
+			if (gamepad1.left_bumper) {
+				clawChainPower = 0.2f;
+			} else if (gamepad1.right_bumper) {
+				clawChainPower = -0.2f;
+			} else {
+				clawChainPower = 0;
+			}
 
-            double rightFrontPower = 0;
-            double rightBackPower = 0;
-            double leftFrontPower = 0;
-            double leftBackPower = 0;
-            
-            rightFrontPower += -(gamepad1.left_stick_y + gamepad2.left_stick_y ) *speed;
-            rightBackPower += -(gamepad1.left_stick_y+gamepad2.left_stick_y)*speed;
-            leftFrontPower += -(gamepad1.left_stick_y+gamepad2.left_stick_y)*speed;
-            leftBackPower += -(gamepad1.left_stick_y+gamepad2.left_stick_y)*speed;
-          
-            //Side Ways Strafe
-            rightFrontPower += -(gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
-            rightBackPower +=  (gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
-            leftFrontPower += (gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
-            leftBackPower += -(gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
-            
-            //Rotation
-            rightFrontPower += -(gamepad1.right_stick_x+gamepad2.right_stick_x)*1.5*speed;
-            rightBackPower += -(gamepad1.right_stick_x+gamepad2.right_stick_x)*1.5*speed;
-            leftFrontPower += (gamepad1.right_stick_x+gamepad2.right_stick_x)*1.5*speed;
-            leftBackPower += (gamepad1.right_stick_x+gamepad2.right_stick_x)*1*speed;
 
-            // Set motor powers
-            arm.setPower(clawMainPower);
-            chain.setPower(clawChainPower);
+			if (gamepad1.dpad_down) {
+				sliderPower = 0.2f;
+			} else if (gamepad1.dpad_up) {
+				sliderPower = -0.2f;
+			} else {
+				sliderPower = 0;
+			}
+			
+			
+			double rightFrontPower = 0;
+			double rightBackPower = 0;
+			double leftFrontPower = 0;
+			double leftBackPower = 0;
+			
+			rightFrontPower += -(gamepad1.left_stick_y + gamepad2.left_stick_y ) *speed;
+			rightBackPower += -(gamepad1.left_stick_y+gamepad2.left_stick_y)*speed;
+			leftFrontPower += -(gamepad1.left_stick_y+gamepad2.left_stick_y)*speed;
+			leftBackPower += -(gamepad1.left_stick_y+gamepad2.left_stick_y)*speed;
+		  
+			//Side Ways Strafe
+			rightFrontPower += -(gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
+			rightBackPower +=  (gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
+			leftFrontPower += (gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
+			leftBackPower += -(gamepad1.left_stick_x+gamepad2.left_stick_x)*speed;
+			
+			//Rotation
+			rightFrontPower += -(gamepad1.right_stick_x+gamepad2.right_stick_x)*1.5*speed;
+			rightBackPower += -(gamepad1.right_stick_x+gamepad2.right_stick_x)*1.5*speed;
+			leftFrontPower += (gamepad1.right_stick_x+gamepad2.right_stick_x)*1.5*speed;
+			leftBackPower += (gamepad1.right_stick_x+gamepad2.right_stick_x)*1*speed;
 
-            rightFront.setPower(rightFrontPower);
-            rightBack.setPower(rightBackPower);
-            leftFront.setPower(leftFrontPower);
-            leftBack.setPower(leftBackPower);
+			// Set motor powers
+			arm.setPower(clawMainPower);
+			chain.setPower(clawChainPower);
+			
+			leftSlider.setPower(sliderPower);
+			rightSlider.setPower(sliderPower);
 
-            // Add telemetry for debugging
-            telemetry.addData("Arm Power", clawMainPower);
-            telemetry.addData("Chain Power", clawChainPower);
-            telemetry.update();
-        }
-    }
+			rightFront.setPower(rightFrontPower);
+			rightBack.setPower(rightBackPower);
+			leftFront.setPower(leftFrontPower);
+			leftBack.setPower(leftBackPower);
+
+			// Add telemetry for debugging
+			telemetry.addData("Arm Power", clawMainPower);
+			telemetry.addData("Chain Power", clawChainPower);
+			telemetry.update();
+		}
+	}
 }
 
-    
+	
