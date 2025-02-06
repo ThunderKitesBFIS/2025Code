f52ace53ac0943590aca726e7ca52cff292c2c1c
canulasu <jakarandainnovation@gmail.com>
2025-02-06 14:17:17 +0100

    Update AUTO_COMPLETE.java

--- Auto/AUTO_COMPLETE.java
+++ Auto/AUTO_COMPLETE.java
@@ -1,1 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
+	private DcMotor rightFront;
+	private DcMotor rightBack;
+	private DcMotor leftFront;
+	private DcMotor leftBack;
+
+public void startMotors() {
+  
+  if (opModeIsActive()) {
+    rightFront.setPower(1);
+    leftFront.setPower(-1);
+    rightBack.setPower(1);
+    leftBack.setPower(-1);
+  }
+}
+
+public void backMotors() {
+  
+  if (opModeIsActive()) {
+    rightFront.setPower(-1);
+    leftFront.setPower(1);
+    rightBack.setPower(-1);
+    leftBack.setPower(1);
+  }
+}
+
+public void stopMotors() {
+  
+  if (opModeIsActive()) {
+    rightFront.setPower(0);
+    leftFront.setPower(0);
+    rightBack.setPower(0);
+    leftBack.setPower(0);
+  }
+}
+
+@TeleOp(name = "AUTO_COMPLETE")
+public class AUTO_COMPLETE extends LinearOpMode {
+
+  startMotors();
+  
+}
