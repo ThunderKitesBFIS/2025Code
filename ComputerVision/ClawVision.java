106035baf2b242f7c6862b645c13486de720a7dd
platonisxe <shrimplockstudio@gmail.com>
2025-02-27 16:57:40 +0100

    Create ClawVision.java

---
+++ ComputerVision/ClawVision.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+// Import necessary FTC libraries
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
